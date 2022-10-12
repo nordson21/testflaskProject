6eaa35b8d71820b11a827f9c7bab39b0ea08db35
nordson <21nordson@gmail.com>
2022-10-12 15:43:49 +0300

    1

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    title = 'Фотограф Юлия Карелина'
+    css = url_for('static', filename='styles/index.css')
+    return render_template('index.html', title=title, css=css)
+@app.route('/about/')
+@app.route('/about')
 
+def about():
+    title = 'Абамне'
+    return 'Йа фатограф'
+
+@app.route('/gallery/<gallery_id>')
+def gallery(gallery_id):
+    title = f'Галерея {gallery_id}'
+    return f'Тут будет галерея номер {gallery_id}'
+
+@app.errorhandler(404)
+def error_404(error):
+    title = f'Error'
+    return 'Такой страницы нет!', 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
