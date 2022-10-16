c9650ea2c58be2226709b2aa854a06db67421507
nordson <21nordson@gmail.com>
2022-10-16 13:02:57 +0300

    error 404 added.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def get_photos_links_from_folder(folder_path="static/images/gallery"):
-    images_urls = [f'{folder_path}/{name}' for name in os.listdir(folder_path)]
+    images_urls = sorted([f'{folder_path}/{name}' for name in os.listdir(folder_path)])
     return images_urls
 
 
@@ -38,10 +38,16 @@ def gallery(gallery_id):
 
 
 @app.errorhandler(404)
-def error_404():
-    # title = f'{error}'
+def error_404(error):
+    title = f'{error}'
     return 'Такой страницы нет!', 404
 
+@app.route('/testgallery')
+def test_gallery():
+    title = 'тест Галерея'
+    css = url_for('static', filename='styles/testgallery.css')
+    photos_list = get_photos_links_from_folder()
+    return render_template('tetstgal-grid.html', title=title, css=css, images_urls=photos_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
